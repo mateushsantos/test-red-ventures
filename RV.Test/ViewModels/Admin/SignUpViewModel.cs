f5fde5a54350260fc0fa1772cb04cddab7f8964b
mateushortenciodossantos <mateushortenciodossantos@gmail.com>
2018-01-16 00:32:50 -0200

    adding error handling attributes in view model too

--- RV.Test/ViewModels/Admin/SignUpViewModel.cs
+++ RV.Test/ViewModels/Admin/SignUpViewModel.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RV.Test.Web.ViewModels.Admin
 {
     public class SignUpViewModel
     {
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Username cannot be empty")]
         public string Username { get; set; }
-        [Required]
         [Compare("ConfirmPassword")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password cannot be empty")]
         public string Password { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password Confirmation cannot be empty")]
         public string ConfirmPassword { get; set; }
     }
 }
