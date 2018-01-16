c1a9c36d25c438f72c1c5f2b1b5339a2a81e3e98
mateushortenciodossantos <mateushortenciodossantos@gmail.com>
2018-01-16 00:30:59 -0200

    widgets is now widget table

--- RV.Test/Context/RvTestContext.cs
+++ RV.Test/Context/RvTestContext.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RV.Test.Web.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RV.Test.Web.Context
 {
